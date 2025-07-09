e453ca0f7e374b5a23a1510b993b868bcb2f0eb5
sotojn <jsoto@terascope.com>
2025-07-09 11:38:08 -0700

    add tests and new ci stuff

---
+++ test/logic.test.js
@@ -0,0 +1,10 @@
+describe('Boolean logic', () => {
+    test('true is truthy', () => {
+      expect(true).toBeTruthy();
+    });
+  
+    test('0 is falsy', () => {
+      expect(0).toBeFalsy();
+    });
+});
+  
---
+++ test/math.test.js
@@ -0,0 +1,9 @@
+describe('Math basics', () => {
+    test('adds numbers', () => {
+      expect(1 + 2).toBe(3);
+    });
+  
+    test('subtracts numbers', () => {
+      expect(5 - 2).toBe(3);
+    });
+});
