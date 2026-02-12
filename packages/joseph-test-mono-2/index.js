bed0a7a18789c1b1093d77a96d63f4597e5398f8
sotojn <jsoto@terascope.com>
2026-02-12 11:47:07 -0700

    make mono1 a dep of mono2

--- packages/joseph-test-mono-2/index.js
+++ packages/joseph-test-mono-2/index.js
@@ -1,3 +1,10 @@
-export default function (num) {
+import mono1 from 'joseph-test-mono-1';
+
+
+export function monoFunc1 (num) {
+    return mono1(num);
+}
+
+export function monoFunc2 (num) {
     return num + 2;
 }
